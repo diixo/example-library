642095353d33d12177ad4271a64ab3ec05f39f84
diixo <anatolii.podoba@gmail.com>
2012-12-31 23:25:44 +0200

    fix indent

--- tests/LogInfo.hpp
+++ tests/LogInfo.hpp
@@ -5,18 +5,17 @@
 #include <sstream>
 #include <iostream>
 
-   class logInfo : public std::ostringstream
+class logInfo : public std::ostringstream
+{
+public:
+   logInfo() = default;
+
+   ~logInfo()
    {
-   public:
-      logInfo() = default;
+      std::lock_guard<std::mutex> guard(_mutexPrint);
+      std::cout << this->str() << std::endl;
+   }
 
-      ~logInfo()
-      {
-         std::lock_guard<std::mutex> guard(_mutexPrint);
-         std::cout << this->str() << std::endl;
-      }
-
-   private:
-      static std::mutex _mutexPrint;
-   };
-
+private:
+   static std::mutex _mutexPrint;
+};
