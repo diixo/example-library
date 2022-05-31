1ffe11c9cc807d73a41f88e76cb72dd13c847c4b
diixo <anatolii.podoba@gmail.com>
2022-06-01 00:25:17 +0300

    Updated src.

--- itc/main-Call.cpp
+++ itc/main-Call.cpp
@@ -1,72 +1,85 @@
 
 #include "itc.hpp"
+#include <memory>
 
-const std::string thread_name1("thread1");
-
-struct A
+namespace ns_CallDemo
 {
-   A(int i, std::string s) : mI(i), mS(s) {}
-   int mI;
-   std::string mS;
-};
+   static const std::string THREAD = "CallDemo_thread";
 
-std::ostream& operator<<(std::ostream &o, std::shared_ptr<A> a)
-{
-   return o << "{i=" << a->mI << ", s=" << a->mS << "}";
+   struct I
+   {
+      virtual void vf(int i) = 0;
+   };
+
+   struct A : I
+   {
+      void func1()
+      {
+         std::cout << itc::currentThreadName() << ": " << "func1()" << std::endl;
+      }
+
+      void func2(int i)
+      {
+         std::cout << itc::currentThreadName() << ": " << "func2(" << i << ")" << std::endl;
+      }
+
+      void func3(std::string s, int i, float f)
+      {
+         std::cout << itc::currentThreadName() << ": " << "func3(" << s << ", " << i << ", " << f << ")" << std::endl;
+      }
+
+      void func4(std::shared_ptr<ns_CallDemo::A> pA);
+
+      void vf(int i) override {
+         std::cout << itc::currentThreadName() << ": " << "vf(" << i << ")" << std::endl;
+      }
+
+      A(int i, std::string s) : mI(i), mS(s) {}
+      int mI;
+      std::string mS;
+   };
+
+   std::ostream& operator<<(std::ostream &o, std::shared_ptr<A> a)
+   {
+      return o << "{i=" << a->mI << ", s=" << a->mS << "}";
+   }
+
+   void A::func4(std::shared_ptr<ns_CallDemo::A> pA)
+   {
+      std::cout << itc::currentThreadName() << ": " << "func4(" << pA << ")" << std::endl;
+   }
 }
 
-struct EventConsumer
-{
-   void func1()
-   {
-      std::cout << itc::currentThreadName() << " --EventConsumer::func1()" << std::endl;
-   }
 
-   void func2(int i)
-   {
-      std::cout << itc::currentThreadName() << " --EventConsumer::func2(" << i << ")" << std::endl;
-   }
+DECLARE_CALL(CALL_func1, ns_CallDemo::THREAD, ns_CallDemo::A, func1)
+DECLARE_CALL(CALL_func2, ns_CallDemo::THREAD, ns_CallDemo::A, func2, int)
+DECLARE_CALL(CALL_func3, ns_CallDemo::THREAD, ns_CallDemo::A, func3, std::string, int, float)
+DECLARE_CALL(CALL_func4, ns_CallDemo::THREAD, ns_CallDemo::A, func4, std::shared_ptr<ns_CallDemo::A>)
 
-   void func3(std::string s, int i, float f)
-   {
-      std::cout << itc::currentThreadName() << " --EventConsumer::func3(" << s << ", " << i << ", " << f << ")" << std::endl;
-   }
-
-   void func4(std::shared_ptr<A> a)
-   {
-      std::cout << itc::currentThreadName() << " --EventConsumer::func4(" << a << ")" << std::endl;
-   }
-};
-
-DECLARE_EVENT(EVENT_func1, EventConsumer, func1)
-DECLARE_EVENT(EVENT_func2, EventConsumer, func2, int)
-DECLARE_EVENT(EVENT_func3, EventConsumer, func3, std::string, int, float)
-DECLARE_EVENT(EVENT_func4, EventConsumer, func4, std::shared_ptr<A>)
-
-// fill events list to call consumer-methods from another thread 
-void startEventDemo()
-{
-   auto consumer = std::make_shared<EventConsumer>();
-
-   itc::invoke(EVENT_func1::Event(consumer));
-   itc::invoke(EVENT_func2::Event(consumer, 5));
-   itc::invoke(EVENT_func3::Event(consumer, "HELLO", 42, 5.5f));
-   itc::invoke(EVENT_func4::Event(consumer, std::make_shared<A>(33, "Hello A")));
-}
-
-DECLARE_STATIC_CALL(STATIC_CALL_ED_start, thread_name1, startEventDemo)
+DECLARE_CALL(CALL_funcV, ns_CallDemo::THREAD, ns_CallDemo::I, vf, int)
 
 int main()
 {
    itc::logInfo() << "main_tid:" << std::this_thread::get_id();
 
-   itc::createEventLoop(thread_name1);
+   //TraceManager::getInstance().registerStrategy("_UNKNOWN_", std::make_shared<DLTTraceStrategy>("DEMO", "Demo"));
 
-   itc::invoke(STATIC_CALL_ED_start::CallStatic());
+   itc::createEventLoop(ns_CallDemo::THREAD);
+
+   auto a = std::make_shared<ns_CallDemo::A>(123, "HELLO A");
+
+   itc::invoke(CALL_func1::Call(a));
+   itc::invoke(CALL_func2::Call(a, 42));
+   itc::invoke(CALL_func3::Call(a, "Demo", 5, 3.14f));
+   itc::invoke(CALL_func4::Call(a, a));
+
+   itc::invoke(CALL_funcV::Call(a, 456));
+
+   //////////////////////////////////////////////////////////////////////////////////////////
 
    while (!itc::_private::Dispatcher::getInstance().isEmpty())
    {
-      auto event = itc::_private::Dispatcher::getInstance().getThreadByName(thread_name1);
+      auto event = itc::_private::Dispatcher::getInstance().getThreadByName(ns_CallDemo::THREAD);
 
       // waiting while all events finish responses by calling from parallel thread.
       if (event && (event->size() == 0))
