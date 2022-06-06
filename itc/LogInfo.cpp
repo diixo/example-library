6b9c1839e6f5dffb2ec649575b85e88f7a365ab4
diixo <anatolii.podoba@gmail.com>
2022-06-06 15:22:00 +0300

    Added LogInfo::LOG_ENABLE

--- itc/LogInfo.cpp
+++ itc/LogInfo.cpp
@@ -2,6 +2,8 @@
 #include "LogInfo.hpp"
 #include "App.hpp"
 
+#define LOG_ENABLE 1
+
 TraceStream::TraceStream(const std::string& threadId, const std::string& prefix, const itc::eTraceMessageLevel logLevel)
    : mBaseStream()
    , mLevel(logLevel)
@@ -12,14 +14,17 @@ TraceStream::TraceStream(const std::string& threadId, const std::string& prefix,
 
 TraceStream::TraceStream(TraceStream&& other)
 {
+#if LOG_ENABLE
    mBaseStream.str(other.mBaseStream.str());
    std::swap(mThreadId, other.mThreadId);
    std::swap(mLevel, other.mLevel);
    std::swap(mPrefix, other.mPrefix);
+#endif
 }
 
 TraceStream::~TraceStream()
 {
+#if LOG_ENABLE
    if (itc::eTraceMessageLevel::logKPI == mLevel)
    {
       //TraceManager::getInstance().logKPI(mThreadId, mLevel, mBaseStream.str());
@@ -29,14 +34,17 @@ TraceStream::~TraceStream()
       //TraceManager::getInstance().trace(mThreadId, mLevel, mPrefix, mBaseStream.str());
       App::GetInstance().trace(mThreadId, mLevel, mPrefix, mBaseStream.str());
    }
+#endif
 }
 
 TraceStream& TraceStream::operator=(TraceStream&& other)
 {
+#if LOG_ENABLE
    mBaseStream.str(other.mBaseStream.str());
    std::swap(mThreadId, other.mThreadId);
    std::swap(mLevel, other.mLevel);
    std::swap(mPrefix, other.mPrefix);
+#endif
    return *this;
 }
 
